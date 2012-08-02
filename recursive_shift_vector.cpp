0e8fed56def27dab40391bc79d0f2831f5ca2e9d
yang yang <smileyang1988@gmail.com>
2012-08-03 07:58:55 +0800

    recursive shift vector

---
+++ recursive_shift_vector.cpp
@@ -0,0 +1,53 @@
+//--yangyang--
+//--2012/08/02--
+
+/**
+* this is a functioin to shift an array by some bits;
+* it is a recursive function
+**/
+#include <iostream>
+using namespace std;
+
+void displayAry_recursive_shift_vector(int *ary, int len){
+	//return ;
+	for(int i=0; i<len; i++)
+		cout<<ary[i]<<'\t';
+	cout<<endl;
+}
+
+void swapAryIndex(int *ary, int start, int end, int swaplen){
+	if(start>=0 && start<end){
+		for(int i=0;i<swaplen;i++){
+			int t=ary[end+i];
+			ary[end+i]=ary[start+i];
+			ary[start+i]=t;
+		}
+	}
+}
+
+void recursive_shift(int *ary, int shift, int len){
+	if(shift==0)
+		return;
+	int left=shift;
+	int right=len-shift;
+	if(left>=right){
+		int mid=left-right;
+		swapAryIndex(ary,0,left,right);
+		//displayAry_recursive_shift_vector(ary,len);
+		recursive_shift(&(ary[right]),mid,mid+right);
+	}else{
+		int mid=right-left;
+		swapAryIndex(ary,0,left+mid,left);
+		//displayAry_recursive_shift_vector(ary,len);
+		recursive_shift(ary,left,left+mid);
+	}
+}
+
+int main(){
+	int ary[] = {0,1,2,3,4,5,6,7,8,9,10};
+	int len=11;
+	int shift=5;
+	recursive_shift(ary,shift,len);
+	displayAry_recursive_shift_vector(ary, len);
+	return 0;
+}
